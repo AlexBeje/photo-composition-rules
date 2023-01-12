e05d9427f7b828af411024e451b837c158422814
AlexBeje <alex.bejenaru93@gmail.com>
2023-01-12 18:04:56 +0100

    Finished the style of the composition rule component.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,19 @@ import CompositionRule from './components/CompositionRule.vue';
 </script>
 
 <template>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <h1>Composition Rules</h1>
-  <CompositionRule msg="Vite + Vue" />
+  <div class="app-container">
+    <!-- <HelloWorld msg="Vite + Vue" /> -->
+    <h1>Composition Rules</h1>
+    <CompositionRule msg="Vite + Vue" />
+  </div>
 </template>
+
+<style scoped>
+h1 {
+  margin: 2rem 0 1rem 0;
+  font-weight: 700;
+}
+.app-container {
+  margin: 1rem;
+}
+</style>
--- src/components/CompositionRule.vue
+++ src/components/CompositionRule.vue
@@ -1,17 +1,26 @@
 <script setup>
-const test = true;
+import { ref } from 'vue';
+import IconForward from '~icons/material-symbols/arrow-forward-ios-rounded';
+const itemOpened = ref(false);
+
+function toggleItem() {
+  itemOpened.value = !itemOpened.value;
+}
 </script>
 
 <template>
   <div class="container">
-    <div class="top-section">
+    <div class="top-section" @click="toggleItem">
       <div class="small-image"></div>
       <h2 class="title">Title</h2>
-      <div class="icon"></div>
+      <div class="icon">
+        <IconForward v-if="itemOpened" class="arrow-rotated" />
+        <IconForward v-else />
+      </div>
     </div>
-    <div class="bottom-section" v-if="test">
-      <div class="large-image">image</div>
+    <div class="bottom-section" v-if="itemOpened">
       <div>description</div>
+      <div class="large-image">image</div>
     </div>
   </div>
 </template>
@@ -25,31 +34,39 @@ const test = true;
     align-items: center;
     justify-content: space-between;
     background: #1c1c1e;
-    padding: 1rem;
+    padding: 0.5rem 1rem;
     .small-image {
-      width: 40px;
-      height: 40px;
+      width: 48px;
+      height: 48px;
       background: #39383d;
+      border-radius: 50%;
     }
     .title {
       flex: 1;
       margin: 0 1rem;
+      font-weight: 400;
     }
     .icon {
-      width: 40px;
-      height: 40px;
-      background: #39383d;
+      width: 48px;
+      height: 48px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .arrow-rotated {
+        transform: rotate(90deg);
+      }
     }
   }
   .bottom-section {
-    background: #39383d;
     padding: 1rem;
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
-    .large-image {
-      background-color: red;
-    }
+    background: linear-gradient(
+      180deg,
+      rgb(18, 18, 18) 0%,
+      rgba(28, 28, 28, 1) 100%
+    );
   }
 }
 </style>
