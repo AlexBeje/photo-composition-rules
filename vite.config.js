d74a2fb7a2ac704dd3c8fc8fbd25b2b5d9203b67
AlexBeje <alex.bejenaru93@gmail.com>
2023-01-14 14:35:16 +0100

    Added pwa for vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Icons from 'unplugin-icons/vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Icons()],
+  plugins: [vue(), Icons(), VitePWA({ registerType: 'autoUpdate' })],
 });
